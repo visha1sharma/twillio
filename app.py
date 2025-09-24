8dae0f9e1ae0c05d52b06e0b7a91f58980f5439e
visha1sharma <vishalsharma04556@gmail.com>
2025-09-24 21:27:16 +0530

    app file

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ def get_all_messages():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-    app.run(port=5000, debug=True)
+    app.run(host="0.0.0.0",port=5000, debug=True)
 
 
 
